73cbdef2c1435d28d117c0834eac0be34c324367
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-03-26 12:21:16 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,6 @@
+// console.log("Hello my name is Akash Singh");
+// document.write("Hello my name is Akash Singh");
+// window.alert("You are below 18");
+// alert("you are below 18");
+// document.getElementById("heading").innerHTML="my name is Akash singh";
+
