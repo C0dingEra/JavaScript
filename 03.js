49352f83c5789360e46c58f91c89958f8aa3db23
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-03-28 14:13:15 +0530

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,38 @@
+//variables
+
+//VAR
+// "use strict";
+// var Name="Akash";
+// var Name="vishal";
+// console.log(Name);
+
+//LET
+// let Name="Akash";
+// Name="vishal";
+// console.log(Name);
+
+//CONST
+// const pi="Akash";
+// pi=300;
+// console.log(Name);
+
+//naming rules for declaring variables
+// 1.cannot start with numbers
+
+// let 5Name="Akash";
+// console.log(Name);
+
+// 2.can use underscore and symbol at the begin
+// let $Name="Akash";
+// console.log($Name);
+
+// 3.you cannot use spaces
+// let first_name="Akash";
+// console.log(first_name);
+
+// 4.you should write in camelCase
+let firstName="Akash";
+console.log(firstName);
+
+
+
