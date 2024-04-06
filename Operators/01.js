35c013a3d7ee21f7932b5416d5777168f8711d7a
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-06 14:43:32 +0530

    Update 01.js

--- Operators/01.js
+++ Operators/01.js
@@ -1,1 +1,25 @@
 
+// Assignment operator
+// let a=5;
+// console.log(a);
+
+// Arithmetic operator
+// let a=3;
+// let b=4;
+// let c=a**b;
+// console.log(c);
+// console.log(a+b);
+
+//increment and decrement
+// let a="5";
+// // console.log(typeof a);
+// let c=+a;
+// console.log(typeof c);
+// // console.log(a++);
+// // console.log(++a);
+// // console.log(a--);
+// // console.log(--a);
+
+//unary negation (-);
+let a=5;
+console.log(-a);
