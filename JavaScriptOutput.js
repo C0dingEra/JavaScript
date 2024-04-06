fa8e20675dfb9b837114df9d3cb2fc65aa8798c9
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-06 15:15:47 +0530

    Delete JavaScriptOutput.js

--- JavaScriptOutput.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello my name is Akash Singh");
-document.write("Hello my name is Akash Singh");
-window.alert("You are below 18");
-alert("you are below 18");
-document.getElementById("heading").innerHTML="my name is Akash singh";
-
