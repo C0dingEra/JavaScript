c0268d6b011f6f876f364528b8ced586cc4dffbd
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-06 14:44:11 +0530

    Add files via upload

---
+++ Operators/02.js
@@ -0,0 +1,41 @@
+//comparison operator
+
+//equality
+// let a=5;
+// let b=6;
+// console.log(a==b);
+
+//strict equality
+// let a="5";
+// let b="5";
+// console.log(a===b);
+
+//inequality
+// let a=6;
+// let b=6;
+// console.log(a!=b);
+
+//strict inequality
+// let a=5; //number
+// let b="5";  //string
+// console.log(a!==b);
+
+//greater than
+// let a=6;
+// let b=5;
+// console.log(a>b);
+
+//greater than equal to
+// let a=5;
+// let b=5;
+// console.log(a>=b);
+
+//less than
+// let a=4;
+// let b=5;
+// console.log(a<b);
+
+//less than or eqaul to
+let a=5;
+let b=5;
+console.log(a<=b);
---
+++ Operators/03.js
@@ -0,0 +1,25 @@
+//logical and(&&)
+// let a=1;
+// let b=3;
+// console.log(a<10 && b<1);
+
+//logical or(||)
+// let a=1;
+// let b=3;
+// console.log(a<10 || b<1);
+
+//logical not(!)
+// let a=1;
+// let b=3;
+// console.log(!(a==b));
+
+//template string
+// let a=5;
+// let name="Akash";
+// console.log(`the value of a is ${a} and my name is ${name}`);
+
+//string concatenation
+let firstName="Akash";
+let lastName="singh";
+// let Name=firstName+" "+lastName;
+// console.log(Name);
