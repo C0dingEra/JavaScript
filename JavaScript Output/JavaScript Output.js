730b04604bfa4b5d333d5771a3fd373476ee85ed
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-06 14:46:19 +0530

    Create JavaScript Output.js

---
+++ JavaScript Output/JavaScript Output.js
@@ -0,0 +1,5 @@
+console.log("Hello my name is Akash Singh");
+document.write("Hello my name is Akash Singh");
+window.alert("You are below 18");
+alert("you are below 18");
+document.getElementById("heading").innerHTML="my name is Akash singh";
