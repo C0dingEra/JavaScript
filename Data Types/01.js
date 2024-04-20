44e3011a18ab02fb68a6cb0807f3444116e31784
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-20 14:24:59 +0530

    Create 01.js

---
+++ Data Types/01.js
@@ -0,0 +1,20 @@
+// number to string
+// let x=5;
+// let y=x+"";
+// console.log(typeof y);
+
+//number to string
+// let x=5;
+// console.log(typeof x)
+// let y=String(x);  //S is capital
+// console.log(typeof y)
+
+//string to number
+// let x="5";
+// console.log(typeof +x);
+
+//string to number
+// let x="5";
+// console.log(typeof x);
+// y=Number(x);  // N is capital
+// console.log(typeof y)
