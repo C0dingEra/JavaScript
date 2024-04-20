52c7336fbdc679c12f762d38d835467a4eeeb892
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-04-20 14:25:50 +0530

    Add files via upload

---
+++ Data Types/02.js
@@ -0,0 +1,43 @@
+//data types
+//primitive data types
+// 1)String
+let Name="Akash";
+console.log(typeof Name);
+// 2)Number
+
+// let x=5;
+// console.log(typeof x);
+// 3)BigInt
+// added in 2020
+let x=25;
+console.log(Number.MAX_SAFE_INTEGER);
+
+//number to BigInt
+// let number=50;
+// let newNumber=BigInt(number);
+// console.log(newNumber);
+
+//BigInt to number
+// let number=50n;
+// console.log(typeof number);
+// let newNumber=Number(number)
+// console.log(typeof newNumber)
+
+// let a=25n;
+// let b=24n;
+// console.log(a+b);
+
+// 4)Boolean (true or false )
+
+// let a=12;
+// let b=20;
+// let c=a>b;
+// console.log(typeof c);
+
+// 5)undefined
+// let name;
+// console.log(typeof name);
+
+// 6)Null
+let name=null;
+console.log(typeof name);
