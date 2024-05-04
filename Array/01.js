4c638efabf2354462351b8872a60bf9899ce6b5e
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-05-04 12:02:43 +0530

    01.js

--- Array/01.js
+++ Array/01.js
@@ -1,1 +1,37 @@
 
+//Array - collection of multiple items(dissimilar)
+//Array - 0 based indexing
+
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// console.log(names);
+
+//Array literals
+
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// console.log(names);
+
+//Array index(accessing element by index)
+
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// console.log(names[4]);
+
+//Array length
+
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false,20];
+// console.log(names.length);
+
+
+//Typeof
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// console.log(typeof names);  //object  (Arrays are object in js)
+
+//Array.isArray
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// console.log(Array.isArray(names));
+
+//Array mutability---> mutable-->changing the original array
+
+// let names=["Akash","Vishal","Ankit","Raju",100,52.54,false];
+// names[1]="Priya";
+// console.log(names);
+
