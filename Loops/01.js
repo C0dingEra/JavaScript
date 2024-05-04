595a1cbad9dc6c16872365f10e1bb435d1b56398
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-05-04 11:45:51 +0530

    01.js

---
+++ Loops/01.js
@@ -0,0 +1,58 @@
+//if else
+
+// let marks=76;
+// if(marks>=75){
+//     console.log("You are passed");
+//     console.log("aap pass ho gye");
+// }
+// else
+//     console.log("you are failed");
+
+//elseif ladder
+
+// let marks=59;
+// if(marks>=90 && marks<=100){
+//     console.log("A");
+// }
+// else if(marks>=80 && marks<90){
+//     console.log("B");
+// }
+// else if(marks>=60 && marks<80){
+//     console.log("C")
+// }
+// else{
+//     console.log("you are fail");
+// }
+
+//nested if else
+// tenthMarks=75;
+// twelthMarks=58;
+
+// if(tenthMarks>=75){
+//     if(twelthMarks>=60){
+//         console.log("you are eligible");
+//     }
+//     else{
+//         console.log("you are not eligible");
+//     }
+// }
+
+let gender="female";
+let age=18;
+if(gender=="male"){
+    if(age>=21){
+        console.log("you are eligible for marriage");
+    }
+    else{
+        console.log("you are not eligible");
+    }
+}
+else{
+    if(age>19){
+        console.log("you are eligible for marriage");
+    }
+    else{
+        console.log("you are not eligible for marriage");
+    }
+}
+
