35d4a741c14e5a5ba6b89755d7d0d3f8546da0f1
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-05-04 12:03:15 +0530

    Add files via upload

---
+++ Array/02.js
@@ -0,0 +1,17 @@
+//Array constructor
+// let names=new Array(2);
+// names[0]="Akash"
+// names[1]="Raju";
+// console.log(names);
+
+// let names=new Array(2);
+// console.log(names);  //2 empty array
+
+
+//with single parameter
+// let names=new Array("Akash");
+// console.log(names);
+
+//with multiple parameter
+let names=new Array("Akash","Raju","Sonu","Pawan");
+console.log(names);
