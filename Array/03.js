5d3a72d738e0239b94c486b52af0a1a5dd927b17
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-05-07 19:06:04 +0530

    Add files via upload

---
+++ Array/03.js
@@ -0,0 +1,31 @@
+//Array methods
+//push
+// let items=[10,"Akash",10.54,false];
+// console.log(items);
+// items.push("Mango"); //add new element at the end
+// items.push("Guava");
+// console.log(items);
+
+//pop
+
+// let items=[10,"Akash",10.54,false];
+// console.log(items);
+// let removedElement=items.pop(); //remove element from end
+// console.log(items);
+// console.log(removedElement);
+
+//unshift
+// let items=[10,"Akash",10.54,false];
+// console.log(items);
+// items.unshift("Mango");  //add new element at the begin
+// console.log(items);
+
+//shift
+let items=[10,"Akash",10.54,false];
+console.log(items);
+let removedElement=items.shift(); //remove element from begin
+console.log(items);
+console.log(items[0]);
+
+//note: push pop is faster than shift and unshift
+
---
+++ Array/04.js
@@ -0,0 +1,20 @@
+//Iterate Array
+//for loop
+// let names=["Akash","Vishal","Ankit","Raju"];
+// console.log(names);
+// for(let i=0;i<names.length;i++){
+// 	console.log(names[i]);
+// }
+
+//for of loop
+// let names=["Akash","Raju","Raman"];
+// for(let z of names){
+// 	console.log(z);
+// }
+
+//for in loop
+// let names=["Vishal","Raman","Raju"];
+// for(let index in names){
+// 	console.log(names[index]);
+// }
+
