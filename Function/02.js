9da3d616542f786b96908d3db7d6152ab47649b2
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2024-06-08 17:40:10 +0530

    Add files via upload

---
+++ Function/02.js
@@ -0,0 +1,30 @@
+//simple Function
+// function Hello(){
+//     console.log("Hello World");
+// }
+// Hello();
+
+//Function Expression
+const Hello=function(){
+    console.log("Hello World");
+}
+Hello();
+
+// const Sum=function(a,b){
+//     return a+b;
+// }
+// const result=Sum(10,10);
+// console.log(result);
+
+//Arrow Function
+
+const Sum=(a,b)=>{
+    return a+b;
+}
+const result=Sum(10,10);
+console.log(result);
+
+// const kyaYeEvenHai=(number)=>number%2==0; //function hata do arrow laga do
+
+// const result=kyaYeEvenHai(11);
+// console.log(result);
