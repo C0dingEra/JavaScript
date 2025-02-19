1688984ca3eac9dfa98886ef7488b5c90e90352b
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2025-02-19 08:20:23 +0530

    Create 01.js

---
+++ execution context/01.js
@@ -0,0 +1,13 @@
+// var firstName="Akash";
+// console.log(firstName);
+
+// var firstName;
+// console.log("firstName", firstName);
+// var firstName = "Akash";
+// console.log(firstName)
+
+console.log("firstName", firstName);
+let firstName = "Akash";
+
+//let and const also hoisted
+// let and const create tdz
