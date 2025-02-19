02ea0126add9be9d49967a0c10d98bf487cd582f
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2025-02-19 08:18:10 +0530

    Add files via upload

---
+++ sets and maps/02.js
@@ -0,0 +1,26 @@
+//maps --> stores key value pair
+
+const map=new Map();
+map.set("Name","Akash");
+map.set(4,"four");
+// console.log(typeof map);
+// console.log(map.size);
+
+// 1)set
+map.set("FirstName","Vishal");
+console.log(map);
+
+// 2)get
+console.log(map.get("FirstName"));
+
+// 3)clear
+// map.clear();
+// console.log(map);
+
+// map.delete(4);
+console.log(map);
+// for(let x of map){
+//     console.log(x);
+// }
+
+
