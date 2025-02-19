1471cfd8f3403b3cb7b0c09d60862cddfad9c750
Evade Won <116268379+C0dingEra@users.noreply.github.com>
2025-02-19 08:19:09 +0530

    Create 01.js

---
+++ oops/01.js
@@ -0,0 +1,16 @@
+let person1={
+    firstName:"Akash",
+    lastName:"singh",
+    roll:54,
+    fullName(){
+        console.log(`my first name is ${this.firstName} and last name is ${this.lastName}`);
+    }
+}
+
+// console.log(person);
+
+// let person2={
+//     firstName:"vikash"
+// }
+
+//repeat
